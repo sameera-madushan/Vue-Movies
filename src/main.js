cec527ae7d1d889fdfc70614f87e5c4dfceda323
sameeraMadushan <sameera@pramixit.com>
2024-03-25 19:17:23 +0530

    Added Responsive CSS

--- src/main.js
+++ src/main.js
@@ -1,18 +1,14 @@
 import './assets/css/main.css'
 import 'swiper/css';
-import 'vue-final-modal/style.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { pinia } from '@/store/store.js';
-import { createVfm } from 'vue-final-modal'
 
 const app = createApp(App)
-const vfm = createVfm()
 
 app.use(router)
 app.use(pinia)
-app.use(vfm)
 
 app.mount('#app')
