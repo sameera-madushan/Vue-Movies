cd57ecd2ca136e7f6d99ceef94391c309ccd442a
Sameera Madushan <sameera.xyz.me@gmail.com>
2025-07-16 15:37:38 +0530

    Update sidebar navigation with Movies and TV Shows

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -8,9 +8,14 @@
             <span class="nav-link-title">Home</span>
         </router-link>
 
-        <router-link to="/explore" class="nav-link" exact active-class="nav-active">
-            <i class='bx bx-compass' ></i>
-            <span class="nav-link-title">Explore</span>
+        <router-link to="/movies" class="nav-link" exact active-class="nav-active">
+            <i class='bx bx-movie-play' ></i>
+            <span class="nav-link-title">Movies</span>
+        </router-link>
+
+        <router-link to="/tv" class="nav-link" exact active-class="nav-active">
+            <i class='bx bx-tv' ></i>
+            <span class="nav-link-title">TV Shows</span>
         </router-link>
     </div>
 </template>
