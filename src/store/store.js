f4355e9cf7f5c0b26a035b7dfa5be0a16edeb57c
Sameera Madushan <sameera.xyz.me@gmail.com>
2025-07-16 15:34:07 +0530

    Add seriesDetails and seasonEpisodes state with related fetch actions

--- src/store/store.js
+++ src/store/store.js
@@ -5,8 +5,10 @@ export const useStore = defineStore({
     id:'main',
     state: () => ({
         heroImages: [],
-        exploreMovies: [],
+        exploreMedia: [],
         searchMovies: [],
+        seriesDetails: null,
+        seasonEpisodes: {},
     }),
     actions:{
         async fetchImages(params, url){
@@ -15,10 +17,10 @@ export const useStore = defineStore({
                 this.heroImages = data.results;
             }
         },
-        async fetchMovies(params, url){
+        async fetchMedia(params, url){
             const data = await API_GET.fetchMovieDb(params, url);
             if (data) {
-                this.exploreMovies = data.results;
+                this.exploreMedia = data.results;
             }
         },
         async fetchSearches(params, url){
@@ -27,6 +29,23 @@ export const useStore = defineStore({
                 this.searchMovies = data.results;
             }
         },
+        async fetchSeriesDetails(fullUrl) {
+            const data = await API_GET.fetchRaw(fullUrl);
+            if (data) {
+                this.seriesDetails = data;
+            }
+        },
+        async fetchSeasonEpisodes(fullUrl) {
+            const data = await API_GET.fetchRaw(fullUrl);
+            if (data && data.episodes) {
+                const matches = fullUrl.match(/tv\/(\d+)\/season\/(\d+)/);
+                    if (matches) {
+                        const seasonNumber = matches[2];
+                        this.seasonEpisodes[seasonNumber] = data.episodes;
+                    }
+            }
+            return data;
+        }
     }
 })
 
