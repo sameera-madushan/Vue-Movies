468403d57026b71bf278c1eff33b6ab9912ae3e1
Sameera Madushan <sameera.xyz.me@gmail.com>
2025-07-18 23:35:57 +0530

    Refactor episode fetching logic and improve loading state management

--- src/components/Episodes.vue
+++ src/components/Episodes.vue
@@ -12,6 +12,7 @@ const props = defineProps({
 
 const store = useStore();
 const loading = ref(true);
+const loadingSeasons = ref(new Set());
 
 const TMDB_IMAGE = import.meta.env.VITE_TMDB_IMAGE;
 const STREAM_URL = import.meta.env.VITE_STREAM_URL;
@@ -22,93 +23,111 @@ const selectedEpisode = ref(null);
 const videoUrl = ref('');
 const modules = [Autoplay, Navigation];
 
-const seasonEpisodes = computed(() => store.seasonEpisodes);
+const seasonEpisodes = ref({});
 
 const filteredSeasons = computed(() => {
   return props.seasons.filter(season => season?.season_number && season.season_number !== 0);
 });
 
 const fetchSeasonEpisodes = async (seasonNumber) => {
+  loadingSeasons.value.add(seasonNumber);
 
   const url = `${TMDB_SERIES_DETAILS}/${props.seriesId}/season/${seasonNumber}`;
   const data = await store.fetchSeasonEpisodes(url);
-  
+
   if (data && data.episodes) {
-    seasonEpisodes.value = {
-      ...seasonEpisodes.value,
-      [seasonNumber]: data.episodes
-    };
+    seasonEpisodes.value[seasonNumber] = data.episodes;
   }
+
+  loadingSeasons.value.delete(seasonNumber);
 };
 
 const fetchAllSeasonEpisodes = async () => {
   loading.value = true;
-  const fetchPromises = filteredSeasons.value.map(season => fetchSeasonEpisodes(season.season_number));
-  await Promise.all(fetchPromises);
-  loading.value = false;
+
+  for (const season of filteredSeasons.value) {
+    fetchSeasonEpisodes(season.season_number);
+  }
+
+  loading.value = false; 
 };
 
-
-onMounted(() => {
-    fetchAllSeasonEpisodes();
-});
+const getEpisodeImage = (episode) => {
+  if (episode.still_path) {
+    return TMDB_IMAGE + episode.still_path;
+  }
+  const fallback = store.seriesDetails?.backdrop_path ?? store.seriesDetails?.poster_path;
+  return fallback ? TMDB_IMAGE + fallback : '/fallback.jpg';
+};
 
 const openModal = (episode) => {
   selectedEpisode.value = episode;
   videoUrl.value = `${STREAM_URL}?video_id=${props.seriesId}&tmdb=1&s=${episode.season_number}&e=${episode.episode_number}`;
   showModal.value = true;
 };
+
+onMounted(fetchAllSeasonEpisodes);
 </script>
 
 <template>
-    <section class="episodes container">
-
-    <div class="loader-container" v-if="loading">
-        <div class="spinner"></div>
+  <section class="episodes container">
+    <div class="loader-container" v-if="loading && !Object.keys(seasonEpisodes).length">
+      <div class="spinner"></div>
     </div>
 
     <template v-else>
-        <div v-for="season in filteredSeasons" :key="season?.id">
+      <div v-for="season in filteredSeasons" :key="season?.id">
         <div class="heading">
-            <h2 class="heading-title">
+          <h2 class="heading-title">
             <span>Season {{ season.season_number }}</span>
-            </h2>
+          </h2>
+        </div>
+
+        <div v-if="loadingSeasons.has(season.season_number)">
+          <div class="spinner"></div>
         </div>
 
         <swiper
-            :breakpoints="{
-                280: { slidesPerView: 1, spaceBetween: 10 },
-                320: { slidesPerView: 2, spaceBetween: 10 },
-                510: { slidesPerView: 1, spaceBetween: 10 },
-                758: { slidesPerView: 3, spaceBetween: 15 },
-                900: { slidesPerView: 4, spaceBetween: 20 }
-            }"
-            :navigation="true"
-            :autoplay="{ delay: 5000, disableOnInteraction: false }"
-            :modules="modules"
+          v-else
+          :breakpoints="{
+            280: { slidesPerView: 1, spaceBetween: 10 },
+            320: { slidesPerView: 2, spaceBetween: 10 },
+            510: { slidesPerView: 1, spaceBetween: 10 },
+            758: { slidesPerView: 3, spaceBetween: 15 },
+            900: { slidesPerView: 4, spaceBetween: 20 }
+          }"
+          :navigation="true"
+          :autoplay="{ delay: 5000, disableOnInteraction: false }"
+          :modules="modules"
         >
-            <swiper-slide
-            v-for="(episode, index) in seasonEpisodes[season.season_number] || []"
-            :key="episode.id"
+          <swiper-slide
+            v-for="(episode, index) in seasonEpisodes[season.season_number]"
+            :key="`${season.season_number}-${episode.episode_number}`"
             :class="{ 'no-margin': index === 0 }"
-            >
+          >
             <div class="movie-box">
-                <img :src="TMDB_IMAGE + episode.still_path" :alt="episode.name" class="movie-box-img" />
-                <div class="box-text">
+              <img
+                :src="getEpisodeImage(episode)"
+                :alt="episode.name"
+                class="movie-box-img"
+              />
+              <div class="box-text">
                 <h2 class="movie-title">{{ episode.name }}</h2>
-                <span class="movie-date">Episode {{ episode.episode_number }} • {{ episode.air_date }}</span>
+                <span class="movie-date">
+                  Episode {{ episode.episode_number }} • {{ episode.air_date }}
+                </span>
                 <a href="#" @click.prevent="openModal(episode)" class="watch-btn play-btn">
-                    <i class='bx bx-right-arrow animate'></i>
+                  <i class="bx bx-right-arrow animate"></i>
                 </a>
-                </div>
+              </div>
             </div>
-            </swiper-slide>
+          </swiper-slide>
         </swiper>
-        </div>
+      </div>
     </template>
 
     <Modal v-model="showModal" :videoUrl="videoUrl">
-        <iframe
+      <iframe
         v-if="videoUrl"
         :src="videoUrl"
         width="100%"
@@ -116,9 +135,9 @@ const openModal = (episode) => {
         frameborder="0"
         allow="autoplay; encrypted-media"
         allowfullscreen
-        ></iframe>
+      ></iframe>
     </Modal>
-    </section>
+  </section>
 </template>
 
 <style scoped>
@@ -130,4 +149,7 @@ const openModal = (episode) => {
   margin-bottom: 0;
 }
 
+.spinner {
+  margin-bottom: 60px;
+}
 </style>
