b05bb61049ccb48c60ed4b79657af049c08524e7
Sameera Madushan <sameera.xyz.me@gmail.com>
2025-07-19 12:16:44 +0530

    Add vue3-toastify for toast notifications and included in main.js

--- src/main.js
+++ src/main.js
@@ -1,13 +1,21 @@
 import './assets/css/main.css'
 import 'swiper/css/navigation';
+import 'vue3-toastify/dist/index.css';
 import 'swiper/css';
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { pinia } from '@/store/store.js';
+import Vue3Toastify from 'vue3-toastify';
 
 const app = createApp(App)
+
+app.use(Vue3Toastify, {
+  autoClose: 3000,
+  position: "top-right",
+  theme: "dark", 
+});
 
 app.use(router)
 app.use(pinia)
